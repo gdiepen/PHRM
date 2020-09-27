e55860dfefbded6053a48389d984e91caf15abb2
Guido Diepen <site-github@guidodiepen.nl>
2020-09-27 14:05:33 +0200

    Changed webpack configuration for development

--- webui/webpack.config.dev.js
+++ webui/webpack.config.dev.js
@@ -1,8 +1,39 @@
 const path = require('path')
 
+
+const { AddDependencyPlugin } =  require("webpack-add-dependency-plugin")
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const app_pages = [
+	{
+		page: 'index',
+		description: '<span class="fa fa-home fa-lg"></span> Daily heart rate'
+	},
+	{
+		page: 'contact',
+		description: '<span class="fa fa-list fa-lg"></span> Heart rate over time'
+	},
+	{
+		page: 'about',
+		description: '<span class="fa fa-info fa-lg"></span> About'
+	}
+]
+
+const app_pages_plugins = app_pages.map( function(page){
+    return new HtmlWebpackPlugin({
+            hash: true,
+            template: "./src/pages/"+page.page+"/tmpl.ejs",
+            inject: true,
+            chunks: [page.page],
+            pagename: page.page,
+            filename: page.page+'.html',
+            description: page.description,
+            all_pages: app_pages
+        })
+})
 
 
 
@@ -34,35 +65,6 @@ module.exports = {
         ignored: [ '**/*.swp' , '*.swp' , 'node_modules/**' ]
     },
 
-    plugins: [
-        new CleanWebpackPlugin(),
-
-
-        new HtmlWebpackPlugin({
-            template: "./src/pages/index/tmpl.ejs",
-            inject: true,
-            chunks: ['index'],
-            filename: 'index.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/about/tmpl.ejs',
-            inject: true,
-            chunks: ['about'],
-            filename: 'about.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/contact/tmpl.ejs',
-            inject: true,
-            chunks: ['contact'],
-            filename: 'contact.html'
-        }),
-
-
-
-        new MiniCssExtractPlugin({
-            filename: 'combined_style.css',
-        }),
-    ],
 
     module: {
         rules: [
@@ -120,7 +122,93 @@ module.exports = {
             },
 
         ]
-    }
+    },
 
+
+    plugins: [ new CleanWebpackPlugin ].concat(app_pages_plugins).concat(
+
+    [new AddDependencyPlugin({ path: "./src/common/navbar.ejs" })]
+
+    ).concat(
+    [
+new class DependencyGraphPlugin {
+  apply(compiler) {
+    compiler.hooks.emit.tap("DependencyGraphPlugin", (compilation) => {
+      let deps = [];
+
+      compilation.modules.forEach(m =>  {
+        if (m.resource) {
+          const file = path.relative('', m.resource)
+          const issuer= m.issuer && m.issuer.resource && path.relative('', m.issuer.resource)
+
+          if(file !== issuer){
+            deps.push([issuer, file]);
+          }
+        }
+        
+      })
+      // Insert this list into the webpack build as a new file asset:
+      const source = `digraph sources {
+        rankdir=LR;
+        ${deps.map(d => `"${d[0]}" -> "${d[1]}";`).join('\n\t')} 
+      }`;
+
+
+    console.log(source)
+      compilation.assets['graph.dot'] = {
+        source: function() {
+          return source;
+        },
+        size: function() {
+          return source.length;
+        }
+      };
+    })
+  }
+}
+
+    ]
+
+
+
+    ).concat([ new MiniCssExtractPlugin({ filename: '[name].css', })])
+
+	/*
+    plugins: [
+        new CleanWebpackPlugin(),
+
+
+        new HtmlWebpackPlugin({
+            template: "./src/pages/index/tmpl.ejs",
+            inject: true,
+            chunks: ['index'],
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/about/tmpl.ejs',
+            inject: true,
+            chunks: ['about'],
+            filename: 'about.html',
+	    option_asdf: "asdf",
+		templateParameters:{
+			'foo': 'bar'
+		}
+
+
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/contact/tmpl.ejs',
+            inject: true,
+            chunks: ['contact'],
+            filename: 'contact.html'
+        }),
+
+
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    */
 
 }
